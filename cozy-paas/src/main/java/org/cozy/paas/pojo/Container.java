42dd17612c9998bc3de28f161a50168ce24bbcb0
cozybz <cozybz@gmail.com>
2014-02-09 13:32:59 +0800

    add Class Container,Host,User

---
+++ cozy-paas/src/main/java/org/cozy/paas/pojo/Container.java
@@ -0,0 +1,32 @@
+package org.cozy.paas.pojo;
+
+public class Container {
+	private String id;
+	private Long hostId;
+	private Long userId;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Long getHostId() {
+		return hostId;
+	}
+
+	public void setHostId(Long hostId) {
+		this.hostId = hostId;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+}
---
+++ cozy-paas/src/main/java/org/cozy/paas/pojo/Host.java
@@ -0,0 +1,23 @@
+package org.cozy.paas.pojo;
+
+public class Host {
+	private String id;
+	private int ip;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public int getIp() {
+		return ip;
+	}
+
+	public void setIp(int ip) {
+		this.ip = ip;
+	}
+
+}
